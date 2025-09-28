e00bd0d594a5bbaa7ab4ba835aca703ab49b1b22
hellshade <midnight3animal@gmail.com>
2025-09-28 20:16:28 +0500

    feat: реализована прокрутка и динамическое поведение кнопок скролла

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import Card from './Card';
 
 const API_BASE_URL = 'http://localhost:1488';
@@ -6,6 +6,53 @@ const API_BASE_URL = 'http://localhost:1488';
 function CardSection({ title, endpoint }) {
     const [characters, setCharacters] = useState([]); // состояние для хранения персонажей
     const [isLoading, setIsLoading] = useState(true); //состояние для индикатора загрузки
+    const [backButtonVisibility, setBackButtonVisibility] = useState(false); //состояние видимости кнопки "Назад"
+    const [forwardButtonVisibility, setForwardButtonVisibility] = useState(true); //состояние видимости кнопки "Вперёд"
+    const cardsRowRef = useRef(null)
+
+    function updateButtonVisibility() {
+        function getCurrentPosition() {
+            const visibleWidth = cardsRowRef.current.clientWidth;
+            const wholeWidth = cardsRowRef.current.scrollWidth;
+            const currentPosition = cardsRowRef.current.scrollLeft;
+            return { visibleWidth, wholeWidth, currentPosition };
+        }
+
+        function handleBackButtonVisibility() {
+            const positionInfo = getCurrentPosition()
+            if (!(positionInfo.currentPosition === 0)) {
+                setBackButtonVisibility(true)
+            }
+            else {
+                setBackButtonVisibility(false)
+            }
+        }
+
+        function handleForwardButtonsVisibility() {
+            const positionInfo = getCurrentPosition()
+            if (positionInfo.currentPosition + positionInfo.visibleWidth === positionInfo.wholeWidth) {
+                setForwardButtonVisibility(false)
+            }
+            else {
+                setForwardButtonVisibility(true)
+            }
+        }
+
+        handleBackButtonVisibility()
+        handleForwardButtonsVisibility()
+    }
+
+    function handleClickForward() {
+        const cardsRowWidth = cardsRowRef.current.offsetWidth;
+        cardsRowRef.current.scrollLeft += cardsRowWidth;
+        updateButtonVisibility()
+    }
+
+    function handleClickBack() {
+        const cardsRowWidth = cardsRowRef.current.offsetWidth;
+        cardsRowRef.current.scrollLeft -= cardsRowWidth;
+        updateButtonVisibility()
+    }
 
     useEffect( () => {
         fetch(`${API_BASE_URL}${endpoint}`)
@@ -23,13 +70,13 @@ function CardSection({ title, endpoint }) {
     return (
         <section className = "cards-section" id = "DATABANK | Andor"> 
 
-                <button className = "scroll-back-button"></button>
+                {backButtonVisibility && <button className = "scroll-back-button" onClick = {handleClickBack}></button>}
 
                 <header className = "cards-section-header">
                     <h2 className = "cards-header-name">Databank | Andor</h2>
                     <a href = "#" className = "cards-see-all">SEE ALL ></a>
                 </header>
-                <div className = "cards-row">
+                <div className = "cards-row" ref = {cardsRowRef} onScroll = {updateButtonVisibility}>
                     {characters.map((character) => (
                         <Card
                             key = {character.name}
@@ -38,7 +85,7 @@ function CardSection({ title, endpoint }) {
                         />
                     ))}
                 </div>
-                <button className = "scroll-forward-button"></button>
+                {forwardButtonVisibility && <button className = "scroll-forward-button" onClick = {handleClickForward} ></button>}
                 </section>
     );
 }
